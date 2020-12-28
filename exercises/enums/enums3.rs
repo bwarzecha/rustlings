430a28b9c459ef24ed46c357d42a68641f444179
bwarzecha <bartosz@warzecha.co.uk>
2020-12-28 08:24:22 +0000

    Rustling part 1

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,11 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    ChangeColor ((u8, u8, u8)),
+    Move (Point),
+    Echo (String),
+    Quit
 }
 
 struct Point {
@@ -36,7 +37,13 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
+        match message {
+            Message::Echo (s) => println!("{}", s),
+            Message::Move (p) => self.position = p,
+            Message::ChangeColor (c) => self.color = c,
+            Message::Quit => self.quit = true,
+            _ => panic!("This doesn't work for me")
+        }
     }
 }
 
