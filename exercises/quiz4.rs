ae7bb430d6bb2a7ff61c887d15ac0f27b89576db
bwarzecha <bartosz@warzecha.co.uk>
2021-01-01 22:09:44 +0000

    Next phase

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -5,7 +5,15 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+macro_rules! my_macro {
+    ($val:expr) => {
+        match $val{
+            "world!" => "Hello world!",
+            "goodbye!" => "Hello goodbye!",
+            _ => panic!("Not working")
+        }
+    };
+}
 
 #[cfg(test)]
 mod tests {
